bcb20085d0efe766d6bb49eaee215c26587e01d4
SincIT <sheenmccullough@Sheens-Air.fios-router.home>
2021-12-28 14:52:27 -0500

    adding supervised model

---
+++ transformer/config.py
@@ -0,0 +1,1 @@
+import transformersTRAIN_BATCH_SIZE = 64VALIDATION_BATCH_SIZE = 1MODEL = 'bert-base-uncased'EPOCHS = 3MAX_LEN = 256TOKENIZER = transformers.BertTokenizer.from_pretrained(MODEL,do_lower_case=True)
